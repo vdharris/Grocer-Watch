d61ffa349ead1655e78fe2962923f01fd9d0ab0d
Vander <vander@Vanders-MacBook-Pro.local>
2023-05-24 00:29:13 -0700

    updated store buttons

--- src/components/QueryBar.js
+++ src/components/QueryBar.js
@@ -4,99 +4,92 @@ import StoreButton from './StoreButton'
 
 
 class QueryBar extends Component {
-  
-  
-//   handleClick = async () => {
-//     try {
-//         const data = await (await fetch(`https://api.fda.gov/food/enforcement.json?search=recalling_firm:%22target%20corporation%22&sort=report_date:desc&limit=10`)).json();
-//         console.log(data);
-//         setData(data);
-//     } catch (err) {
-//         console.log(err.message)
-//     }
-// }
-constructor(props) {
-  super(props);
-  this.state = {
-    url: 'https://api.fda.gov/food/enforcement.json?sort=report_date:desc&limit=5',
-    recalls: [],
-    stores: ['target','kroger','aldi','walmart','publix','costco','whole foods','albertsons','dollar general','trader joe'],
-  };
-  this.findRecall = this.findRecall.bind(this);
-}
 
-//create fetch request for server items and update the this.state with the items fetched into the urls array
-componentDidMount() {
-  fetch(this.state.url)
-  .then( response => response.json())
-  .then((data) => {
-    // console.log(data);
-    this.setState({
-      recalls: data.results
-    });
-  })
-}
 
-componentDidUpdate(prevProps, prevState){
-  if(prevState.url !== this.state.url){
+  //   handleClick = async () => {
+  //     try {
+  //         const data = await (await fetch(`https://api.fda.gov/food/enforcement.json?search=recalling_firm:%22target%20corporation%22&sort=report_date:desc&limit=10`)).json();
+  //         console.log(data);
+  //         setData(data);
+  //     } catch (err) {
+  //         console.log(err.message)
+  //     }
+  // }
+  constructor(props) {
+    super(props);
+    this.state = {
+      url: 'https://api.fda.gov/food/enforcement.json?sort=report_date:desc&limit=5',
+      recalls: [],
+      stores: ['Target', 'Kroger', 'Aldi', 'Walmart', 'Publix', 'Costco', 'Whole Foods', 'Albertsons', 'Dollar General', 'Trader Joe', 'Recent Recalls'],
+    };
+    this.findRecall = this.findRecall.bind(this);
+    console.log('construction')
+  }
+
+  //create fetch request for server items and update the this.state with the items fetched into the urls array
+  componentDidMount() {
     fetch(this.state.url)
-    .then( response => response.json())
-    .then((data) => {
-      // console.log(data);
-      this.setState({
-        recalls: data.results
-      });
-    })
+      .then(response => response.json())
+      .then((data) => {
+        // console.log(data);
+        this.setState({
+          recalls: data.results
+        });
+      })
   }
-}
 
-  findRecall(store){
-    console.log(`You hit button for ${store}`);
-    if(store === 'target'){
-      this.setState({
-        url: 'https://api.fda.gov/food/enforcement.json?search=recalling_firm:%22target%20corporation%22&sort=report_date:desc&limit=5'
-      })
-      // console.log(this.state.url);
-    }
-    if(store === 'whole foods'){
-      this.setState({
-        url: 'https://api.fda.gov/food/enforcement.json?search=recalling_firm:%22whole%20foods%20market%22&sort=report_date:desc&limit=5'
-      })
-      // console.log(this.state.url);
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.url !== this.state.url) {
+      fetch(this.state.url)
+        .then(response => response.json())
+        .then((data) => {
+          // console.log(data);
+          this.setState({
+            recalls: data.results
+          });
+        })
     }
   }
-  
-  
+
+  findRecall(store) {
+    console.log(`You hit button for ${store}`);
+    if (store !== 'Recent Recalls') {
+      this.setState({
+        url: `https://api.fda.gov/food/enforcement.json?search=distribution_pattern:${store}&sort=report_date:desc&limit=25`
+      })
+      // console.log(this.state.url);
+    }else{
+      this.setState({
+        url: 'https://api.fda.gov/food/enforcement.json?sort=report_date:desc&limit=50'
+      })
+    }
+  }
+
+
   render() {
     const buttons = [];
+    this.state.stores.forEach(ele =>
+      buttons.push(
+        <button type='button' className='storeButton' onClick={() => this.findRecall(ele)}>{ele}</button>
+      )
+    );
 
     const items = [];
-      for (let i = 0; i < this.state.recalls.length; i++){
-        // console.log(this.state.recalls[i]);
-        items.push(<RecallResults key={`item${i}`} value={this.state.recalls[i]} />);
-      }
-    
+    for (let i = 0; i < this.state.recalls.length; i++) {
+      // console.log(this.state.recalls[i]);
+      items.push(<RecallResults key={`item${i}`} value={this.state.recalls[i]} />);
+    }
+
 
     return (
       <div className='queryBarContainer'>
         {/* {items} */}
-        <h2>QueryBar Finally Here!</h2>
+        <h2>Store Selector</h2>
         <div className='storesContainer'>
-          {/* <select id="stores">
-            <option value="target corporation">Target</option>
-            <option value="whole foods market">Whole Foods</option>
-          </select> */}
-          <button type='button' className='storeButton'
-          onClick={() => this.findRecall('target')}>Target</button>
-          <button type='button' className='storeButton'
-          onClick={() => this.findRecall('whole foods')}
-          >Whole Foods</button>
-          {/* {this.state.stores.map((val,index) =>{
-            <StoreButton key={`storebutton${index}`} value={val}></StoreButton>
-          })} */}
+          {buttons}
         </div>
         <div className='recallFeed'>
-        {items}
+          {items}
         </div>
       </div>
     );
--- src/components/RecallResults.js
+++ src/components/RecallResults.js
@@ -6,12 +6,13 @@ class RecallResults extends Component {
 
 render(){
     const { report_date, recalling_firm, product_description} = this.props.value;
+    const product_description2 = product_description.slice(0,150);
+    const report_date2 = `${report_date.slice(0,4)}-${report_date.slice(4,6)}-${report_date.slice(6,8)}`
     return (
         <div className='recallItem'>
-            In Recall Results
-            <p id='date'>{report_date}</p>
+            <p id='date'>Date of recall: {report_date2}</p>
             <p id='firm'>{recalling_firm}</p>
-            <p id='product_info'>{product_description}</p>
+            <p id='product_info'>{product_description2}</p>
         </div>
     )
 }
